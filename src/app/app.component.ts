7ce3f2aca4b79eec032617e82b55f53719f9c80b
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-26 13:51:06 +0300

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: "<router-outlet></router-outlet>",
+  template: `<div class="container" style="margin-top: 30px;">
+  <h1>Todo App</h1>
+  <hr>
+  <div class="form-group">
+    <label for="work">Yapılacak İş</label>
+    <input type="text" class="form-control"
+    id="work" name="work">
+  </div>
+  <div class="form-group mt-2">
+  `
 
 })
 export class AppComponent {
